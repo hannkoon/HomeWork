969b9ab4a659405c65dc9d2cf48775f1cfd03876
hannkoon <hannkoon@gmail.com>
2019-11-13 14:20:35 +0200

    js first homework

---
+++ lesson 17/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let hours = 3;
+let min = 60;
+let sec = 60;
+
+let time = hours*min*sec;
+console.log(time);
+
+let num = 1;
+num+=12;
+num-=14;
+num*=5;
+num/=7;
+num+=1;
+num-=1;
+alert(num);
+
+let a = 10;
+let b = 2;
+let c = 5;
+let sum = a + b + c;
+console.log(sum);
+
+a = 17;
+b = 10;
+c = a - b;
+let d = 7;
+let result = c + d;
+console.log(result);
+
+let name = 'Привет, Аня!'
+console.log(name);
